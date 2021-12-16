5f76da8ada2bba7683c36ccaf1cfadb2d59f866a
dkammer <kammer@htw-dresden.de>
2021-12-16 14:42:42 +0100

    sessionid in google forms iframe

--- src/app/study/follow-up/follow-up.component.ts
+++ src/app/study/follow-up/follow-up.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 import { DataService } from 'src/app/shared/data.service';
+import { LogPublishersService } from 'src/app/shared/log-publishers.service';
 
 @Component({
   selector: 'app-follow-up',
@@ -9,8 +11,10 @@ import { DataService } from 'src/app/shared/data.service';
 export class FollowUpComponent {
 
   appState: number = 11;
+  formUrl: SafeResourceUrl;
 
-  constructor(private dataService: DataService ) { 
+  constructor(private dataService: DataService, private logPublisher: LogPublishersService, private sanitizer: DomSanitizer) { 
     this.dataService.setAppState(this.appState);
+    this.formUrl = this.sanitizer.bypassSecurityTrustResourceUrl("https://docs.google.com/forms/d/e/1FAIpQLSfhNsZGf-QtCfsOBqPXFDcXlrJtaeshBU3oIVN0_i4PgcThSA/viewform?embedded=true&entry.178224731=" + this.logPublisher.getSessionID());
   }
 }
--- src/app/study/preliminary/preliminary.component.ts
+++ src/app/study/preliminary/preliminary.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 import { DataService } from 'src/app/shared/data.service';
+import { LogPublishersService } from 'src/app/shared/log-publishers.service';
 
 @Component({
   selector: 'app-preliminary',
@@ -9,8 +11,11 @@ import { DataService } from 'src/app/shared/data.service';
 export class PreliminaryComponent {
 
   appState: number = 3;
+  formUrl: SafeResourceUrl; 
 
-  constructor(private dataService: DataService ) { 
+  constructor(private dataService: DataService, private logPublisher: LogPublishersService, private sanitizer: DomSanitizer) { 
     this.dataService.setAppState(this.appState);
+    // TODO: Change to actual pre-filled forms link!
+    this.formUrl = this.sanitizer.bypassSecurityTrustResourceUrl("https://docs.google.com/forms/d/e/1FAIpQLSehfggOwGqnutR4kkl8QKGTnC3MLVervPPYjUNEOlN9l6Roig/viewform?embedded=true&entry.1280742886=" + this.logPublisher.getSessionID());
   }
 }
